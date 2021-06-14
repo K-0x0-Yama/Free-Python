8c110ebfb57b1937e643303f71c6490160309d9a
K-0x0-Yama <learning.never.exhausts.the.mind9@gmail.com>
2021-06-14 11:03:41 +0900

    変数から文字列関係の組み込み関数について学習した

--- assignment.py
+++ assignment.py
@@ -13,18 +13,23 @@ Case sensitive(Hellとhellは別の変数)
 
 '''
 
-
+# 変数
 hello = "konnichiwa"
 world = "sekai"
 print(hello + world)
 
 
 # format
-
-
 # 文字列メソッドの呼び方
 print("{} {}".format(hello, world))
 
 name = "John"
 print("Hey, {}!! How are you doing?".format(name))
+print(f"Hey, {name}!! How are you doing?")  # fstringを使って書き直して見る
 
+# fstring python3.5から推奨されたものでformatに比べると内部的に速い処理をしている
+print(f"{hello} {world}")
+
+
+print(type(name))
+print(id(name))
