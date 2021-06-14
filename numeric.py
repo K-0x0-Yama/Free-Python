fff8e0354e73b3e43351e63cf37228fe3119086b
K-0x0-Yama <learning.never.exhausts.the.mind9@gmail.com>
2021-06-15 07:46:34 +0900

    演算子関係のことについて学習した

---
+++ numeric.py
@@ -0,0 +1,16 @@
+# 数値型(Numeric): integer, float, complex
+
+# int (integer, 整数) -3, -2, -1, 0, 1, 2, 3, 100
+print(type(100))
+
+
+# float(浮動小数点)
+print(type(0.1))
+print(0.1 + 0.1 + 0.1)  # 0.3になるはず→ 結果は0.3000000000004
+
+
+
+
+
+
+
